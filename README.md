430f71216f8144fc38599c0b4862fca0547ab75b
Michael Lewandowski <milewgit@gmail.com>
2014-10-02 15:45:59 -0400

    Update README with link to test, remove superfluous test

--- README.md
+++ README.md
@@ -6,8 +6,6 @@ need to be updated.  'put' should be used when updating all attributes of a reso
 
 Rails allows 'put' to behave like 'patch'.
 
-A detailed discussion is [here](https://github.com/rails/rails/pull/505).
+A detailed discussion is [here](https://github.com/rails/rails/pull/505).  A conclusion is [here](http://weblog.rubyonrails.org/2012/2/26/edge-rails-patch-is-the-new-primary-http-method-for-updates/).
 
-A conclusion is [here](http://weblog.rubyonrails.org/2012/2/26/edge-rails-patch-is-the-new-primary-http-method-for-updates/).
-
-This tiny rails project demonstrates how Rails allows 'put' to update only some attributes.  See [this]() test.
+This tiny rails project demonstrates how Rails allows 'put' to update only some attributes.  See [this](https://github.com/milewgit/restful-rails-lab/blob/master/test/integration/restful_test.rb) test.
--- test/integration/restful_test.rb
+++ test/integration/restful_test.rb
@@ -3,38 +3,6 @@ require 'test_helper'
 class RestfulTest < ActionDispatch::IntegrationTest
 
   
-  test 'RESTful get' do
-    # setup
-    user = User.create!({first_name: 'First', middle_name: 'Middle', last_name: 'Last'})
-    
-    # run
-    get "/users/#{user.id}.json"
-    
-    # verify
-    user_attributes = JSON.parse(response.body)
-    assert_equal 'First', user_attributes['first_name']
-    assert_equal 'Middle', user_attributes['middle_name']
-    assert_equal 'Last', user_attributes['last_name']
-  end
-
-  
-  test 'RESTful post' do
-    # setup
-    new_user_attributes = { first_name: 'First', middle_name: 'Middle', last_name: 'Last' }
-    request_headers = { "Accept" => "application/json", "Content-Type" => "application/json" }
-    
-    # run
-    post "/users.json", new_user_attributes.to_json, request_headers
-    
-    # verify
-    new_user_id = JSON.parse(response.body)['id']
-    new_user = User.find(new_user_id)
-    assert_equal 'First', new_user.first_name
-    assert_equal 'Middle', new_user.middle_name
-    assert_equal 'Last', new_user.last_name
-  end
-
-  
   test 'RESTful put' do
     # setup
     user = User.create!({first_name: 'First', middle_name: 'Middle', last_name: 'Last'})
@@ -61,7 +29,7 @@ class RestfulTest < ActionDispatch::IntegrationTest
     # run
     #
     # NOTE: according to the RESTful spec, this should fail since all of a resource's 
-    # attributes are to be supplied when using 'put'
+    # attributes are to be supplied when using 'put' but here we only supplied 'first_name'.
     put "/users/#{user.id}.json", changes.to_json, request_headers
     
     # verify
